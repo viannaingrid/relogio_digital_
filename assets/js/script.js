71388d8bf6a31b8d4105354c3491a20226dcadf1
viannaingrid <ingrid.vianna1@outlook.com>
2024-08-10 22:17:02 -0300

    update hours, minutes and seconds

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,4 +7,12 @@ const relogio = setInterval(function time(){
     let hr = dateToday.getHours();
     let min = dateToday.getMinutes();
     let seg = dateToday.getSeconds();
+
+    if( hr < 10) hr = '0' + hr;
+    if( min < 10) min = '0' + min;
+    if( seg < 10) seg = '0' + seg;
+
+    horas.textContent = hr;
+    minutos.textContent = min;
+    segundos.textContent = seg;
 })
